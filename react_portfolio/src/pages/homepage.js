439af62468df18dae4dd3c75469837084beb7bd6
Jason Vissage <92236402+JasonVissage@users.noreply.github.com>
2022-03-30 14:35:23 -0500

    content shows on page again

---
+++ react_portfolio/src/pages/homepage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Contact from '../components/Contact';
+import About from '../components/About';
+
+function Homepage() {
+  return (
+    <div>
+      <About />
+      <Contact />
+    </div>
+  );
+}
+
+export default Homepage;
